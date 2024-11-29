16001b43f1e81bb20c086aa10c140ea113b453d3
Parker Baird-Ashmore <parker.baird-ashmore@stu.ocsb.ca>
2024-11-29 15:12:32 +0000

    hi

--- assignment.py
+++ assignment.py
@@ -1,6 +1,13 @@
-def calculatePi(digits):
-  result = 0
-  return result
-
-number_of_digits = input()
-print(calculatePi(number_of_digits))
+word = (input())
+if word.isdigit():
+    word = int(word)
+    if word < 0:
+        print("This program does not input symbols, letters, decimals, or negative integers try again.")
+    i = 0
+    j = 1
+    for n in range(word+1):
+        i += j*((4/(2*n+1)))
+        j *= -1
+    print(i)
+else:
+    print("This program does not input symbols, letters, decimals, or negative integers try again.")
